19fd1690cc36daaa286faa1ef4c5163ebb1d8c25
Misha <astaffers@gmail.com>
2019-09-22 00:29:06 -0400

    File stuff

---
+++ components/constants.js
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: "AIzaSyCIV2Bv_heC_uS5WINrKihEtRmkXp48ZFI",
+  authDomain: "busapp-ed656.firebaseapp.com",
+  databaseURL: "https://busapp-ed656.firebaseio.com",
+  projectId: "busapp-ed656",
+  storageBucket: "",
+  messagingSenderId: "871466590627",
+  appId: "1:871466590627:web:290431ee57f48561bc7a16"
+}
